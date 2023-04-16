0d3918327e509bc98268f90bad2cabdc2c3cf1e1
PascalUrch <pascalurch94@gmail.com>
2023-04-16 15:23:27 -0400

    first commit

--- app.js
+++ app.js
@@ -2,7 +2,7 @@ const express = require('express');
 const app = express();
 const port = 8080;
 
-app.get('/', (req, res) => res.send('This is a Javascript file!'));
+app.get('/', (req, res) => res.send('This script Was updated by the Admin'));
 
 app.listen(port);
 console.log(`App running on http://localhost:${port}`);
